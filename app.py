0fef11c74b1e103c7f056bd221d9766d90aca10f
Carlos Hernandez-Vaquero <carlos.hernandez.vaquero@gmail.com>
2019-12-23 18:23:24 +0100

    Starting the web app with Flask and jinja2 templates

--- app.py
+++ app.py
@@ -1,2 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, request, url_for, redirect
 
+app = Flask(__name__)
+posts = {
+    0: {
+        "post_id": 0,
+        "title": "Hello world!",
+        "content": "This is some better random text!"
+    }
+}
+
+
+@app.route("/")
+def home():
+    return "Hello world!"
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = posts.get(post_id)
+    if not post:
+        return render_template("404.html", message=f"A post with id {post_id} does not exist")
+    else:
+        return render_template("post.html", post=post)
+
+
+@app.route("/post/form")
+def form():
+    return render_template("create.html")
+
+
+@app.route("/post/create", methods=["POST"])
+def create():
+    title = request.form.get("title")
+    content = request.form.get("content")
+    post_id = len(posts)
+    posts[post_id] = {"post_id": post_id, "title": title, "content": content}
+    return redirect(url_for("post", post_id=post_id))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
